eadc8f9657ae15a734300830e8de20011d8489fb
Nick <nick.azurin@gmail.com>
2022-05-29 18:53:13 -0700

    drafted create page. Created state object to hold track information with proper updating

--- src/components/TrackListItem/TrackListItem.js
+++ src/components/TrackListItem/TrackListItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Pill from "../PIll/Pill";
 
 import "./TrackListItem.scss";
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Create.scss";
 
 function Create() {
+  const [track, setTrack] = useState({
+    name: "",
+    artist: "",
+    producers: "",
+    mixengineer: "",
+    masterengineer: "",
+    releasedate: "",
+    imagesrc: "",
+    description: "",
+    genres: [],
+    elements: [],
+  });
+
+  const updateTrack = (e) => {
+    setTrack({
+      ...track,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
-    <div>
+    <div className="create-page">
       <p>This is the post page</p>
+      <label>Track Name</label>
+      <input
+        type="text"
+        value={track.name}
+        onChange={updateTrack}
+        name="name"
+      ></input>
+      <br></br>
+      <label>Artist</label>
+      <input
+        type="text"
+        value={track.artist}
+        onChange={updateTrack}
+        name="artist"
+      ></input>
+      <br></br>
+      <label>Producers</label>
+      <input
+        type="text"
+        value={track.producers}
+        onChange={updateTrack}
+        name="producers"
+      ></input>
+      <br></br>
+      <label>Mix Engineer</label>
+      <input
+        type="text"
+        value={track.mixengineer}
+        onChange={updateTrack}
+        name="mixengineer"
+      ></input>
+      <br></br>
+      <label>Master Engineer</label>
+      <input
+        type="text"
+        value={track.masterengineer}
+        onChange={updateTrack}
+        name="masterengineer"
+      ></input>
+      <br></br>
+      <label>Release</label>
+      <input
+        type="date"
+        value={track.releasedate}
+        onChange={updateTrack}
+        name="releasedate"
+      ></input>
+      <br></br>
+      <label>Image Src</label>
+      <input
+        type="text"
+        value={track.imagesrc}
+        onChange={updateTrack}
+        name="imagesrc"
+      ></input>
+      <div>{JSON.stringify(track)}</div>
     </div>
   );
 }
